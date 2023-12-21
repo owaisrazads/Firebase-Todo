f9674846cafe886dcbf4254bfd779798bbeef131
unknown <oraza7969@gmail.com>
2023-12-21 10:57:25 -0800

    firebase todo

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //import all item
 
 import { createUserWithEmailAndPassword  } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { auth, db} from "./config.js";
+import { auth, db, storage } from "./config.js";
 import { collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js"; 
+import { ref, uploadBytes, getDownloadURL } from 'https://www.gstatic.com/firebasejs/9.10.0/firebase-storage.js'
+
 
 
 
@@ -10,38 +12,44 @@ const form = document.querySelector('#form')
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const userName = document.querySelector('#names')
+const img = document.querySelector('#img')
+
 
 //create user signup
 
-form.addEventListener('submit', (event)=>{
-    event.preventDefault()
-    createUserWithEmailAndPassword(auth, email.value, password.value)
-  .then(async(userCredential) => {
-    const user = userCredential.user;
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  createUserWithEmailAndPassword(auth, email.value, password.value)
+      .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          const file = img.files[0]
+          const storageRef = ref(storage, email.value);
+          uploadBytes(storageRef, file).then(() => {
+              getDownloadURL(storageRef).then((url) => {
+                  addDoc(collection(db, "user"), {
+                      name: name.value,
+                      email: email.value,
+                      uid: user.uid,
+                      profileUrl: url
+                  }).then((res) => {
+                      console.log(res);
+                      window.location = 'index.html'
+                  }).catch((err) => {
+                      console.log(err);
+                  })
+              })
+          });
 
-    //add docs or data bhejwa rhy hn
-    
-    try {
-        const docRef = await addDoc(collection(db, "users"), {
-          names: userName.value,
-          email: email.value,
-          password: password.value,
-          uid: user.uid
-    });
-    
-        console.log("Document written with ID: ", docRef.id);
-      } catch (e) {
-        console.error("Error adding document: ", e);
-      }
-    window.location = 'login.html'
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    console.log(errorMessage);
-    // ..
-  });
-})
+      })
+      .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorMessage);
+          alert(errorCode)
+          register.innerHTML = "Register"
+      });
 
+
+});
 
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,3 +19,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
